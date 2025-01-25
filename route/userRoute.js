015a654af417a27abff19f771e1ad23f429ab61c
Anchal Yadav <anchal.code@gmail.com>
2025-01-25 16:38:34 +0530

    changes

--- route/userRoute.js
+++ route/userRoute.js
@@ -1,6 +1,9 @@
 const express=require('express')
 const {getData,saveData,putData,deleteData}=require('../controllers/userControllers')
 const userRoute=express.Router()
+userRoute.get("/",function(){
+     document.write("Welcome to Node.js!");
+})
 userRoute.get("/getData",getData)
 userRoute.post("/postData",saveData)
 userRoute.put("/PutData/:_id",putData)
